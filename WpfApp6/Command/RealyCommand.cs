10e0d521f6da66360760cb410d5c0a1f903486b3
Mirtalib <108764438+Mirtalib@users.noreply.github.com>
2022-11-01 22:43:09 +0400

    First Commit

---
+++ WpfApp6/Command/RealyCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp6.Command
+{
+    public class RealyCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        private readonly Action<object?>? _execute;
+        private readonly Predicate<object?>? _canExecute;
+
+
+        public RealyCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
+        {
+            ArgumentNullException.ThrowIfNull(execute, nameof(execute));
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+            => _canExecute is null || _canExecute.Invoke(parameter);
+
+        public void Execute(object? parameter) 
+            => _execute?.Invoke(parameter);
+    }
+}
